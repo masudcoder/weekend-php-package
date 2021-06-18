04b70a7255d55ef3c0c4e514c4fca1bfc3b98f1f
Masuduzzaman <ts-masud.masuduzzama@rakuten.com>
2021-06-19 07:44:28 +0900

    Namespace Explained

---
+++ tests/Temp1/WeekendTest3.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Any\Name\You\Want;
+
+
+use Masud\WeekendPackage\Weekend;
+//Masud\WeekendPackage is mapped to srcCode, so it includes from there.
+
+use PHPUnit\Framework\TestCase;
+
+class WeekendTest3 extends TestCase
+{
+
+    public function testValue(): void
+    {
+        $ob = new Weekend();
+
+        $this->assertEquals(true, $ob->isWeekend('2021-06-19'));//sat
+        $this->assertNotEquals(true, $ob->isWeekend('2021-06-18'));//fri
+        $this->assertEquals(true, $ob->isWeekend('2021-06-20'));//sun
+    }
+
+}
